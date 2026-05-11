1b0c0af4dad0ea080756794b3dfdaaf3ddbd11e7
Kurata Yuki <k024g0109@g.neec.ac.jp>
2026-05-11 11:38:30 +0900

    Blenderのシーン情報を走査してコンソール出力する処理の追加

--- blender_addon/level_editor.py
+++ blender_addon/level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 # Blenderに登録するアドオン情報
 bl_info = {
@@ -25,6 +26,7 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
         self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
         self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
 
     # 既存のメニューにサブメニューを追加
     def submenu(self, context):
@@ -55,12 +57,39 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
         bpy.ops.mesh.primitive_ico_sphere_add()
         print("ICO球を生成しました。")
         return {'FINISHED'}
+    
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    # メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        print("シーン情報をExportします")
+        # シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            trans, rot, scale = object.matrix_local.decompose()
+            rot = rot.to_euler()
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            print("Trans(%f, %f, %f)" % (trans.x, trans.y, trans.z) )
+            print("Rot(%f, %f, %f)" % (rot.x, rot.y, rot.z) )
+            print("Scale(%f, %f, %f)" % (scale.x, scale.y, scale.z) )
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+        print("シーン情報をExportしました")
+        self.report({'INFO'}, "シーン情報をExportしました")
+        return {'FINISHED'}
 
 # Blenderに登録するクラスのリスト
 classes = (
+    TOPBAR_MT_my_menu,
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
-    TOPBAR_MT_my_menu,
+    MYADDON_OT_export_scene,
 )
 
 # アドオン有効化時コールバック
